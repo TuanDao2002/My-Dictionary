72227f9fa013e15ff5c93cd5f6149bec926b2e0e
TuanDao2002 <tuan09092002@gmail.com>
2022-08-30 21:54:20 +0700

    add folders

---
+++ My Dictionary/Model/Word.swift
@@ -0,0 +1,8 @@
+//
+//  Word.swift
+//  My Dictionary
+//
+//  Created by Tuan Dao on 30/08/2022.
+//
+
+import Foundation
---
+++ My Dictionary/ViewModel/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  My Dictionary
+//
+//  Created by Tuan Dao on 30/08/2022.
+//
+
+import Foundation
