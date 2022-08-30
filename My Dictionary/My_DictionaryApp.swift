06c0051b68827581ddc5f559e045b2ca0fca0399
TuanDao2002 <tuan09092002@gmail.com>
2022-08-30 20:38:51 +0700

    first commit

---
+++ My Dictionary/My_DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_DictionaryApp.swift
+//  My Dictionary
+//
+//  Created by Tuan Dao on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct My_DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
