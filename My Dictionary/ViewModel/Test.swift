3f83ed184b22bc380e3b75b3f0482686612db27b
TuanDao2002 <tuan09092002@gmail.com>
2022-09-01 23:09:30 +0700

    add login

--- My Dictionary/ViewModel/Test.swift
+++ My Dictionary/ViewModel/Test.swift
@@ -11,16 +11,37 @@ struct Test: View {
     @EnvironmentObject var userVM: UserViewModel
     @State private var msg: String = ""
     @State private var status: Int = -1
+    @State private var user: User?
+    
+    @State private var username: String = ""
+    @State private var password: String = ""
+
     var body: some View {
         VStack {
+            TextField("Username", text: $username)
+            TextField("Password", text: $password)
+
             Button("Register"){
                 Task {
-                    userVM.register(username: "tuan", password: "09092002") { msg, status in
+                    userVM.register(username: username, password: password) { msg, status in
                         self.msg = msg
                         self.status = status
                     }
                 }
             }
+                        
+            Button("Login"){
+                Task {
+                    userVM.login(username: username, password: password) { msg, status, user in
+                        self.msg = msg
+                        self.status = status
+                        
+                        userVM.saveUser(user: user)
+                        self.user = userVM.getUser()
+                    }
+                }
+            }
+            
             if (!msg.isEmpty) {
                 Text("Message: \(msg)")
             }
@@ -28,6 +49,14 @@ struct Test: View {
             if (status != -1) {
                 Text("Status: \(status)")
             }
+            
+            if (user != nil) {
+                Text("UserId: \(user?.id ?? "undefined")")
+                Text("UserId: \(user?.username ?? "undefined")")
+            }
+        }
+        .onAppear() {
+            self.user = userVM.getUser()
         }
     }
 }
--- My Dictionary/ViewModel/UserViewModel.swift
+++ My Dictionary/ViewModel/UserViewModel.swift
@@ -14,7 +14,29 @@ struct Response: Decodable {
 let api = "https://cosc2659dictionary.herokuapp.com/api"
 
 final class UserViewModel: ObservableObject {
-    @Published var user: User?
+    func getUser() -> User?{
+        if let userData = UserDefaults.standard.data(forKey: "user") {
+            do {
+                return try JSONDecoder().decode(User.self, from: userData)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        return nil
+    }
+    
+    func saveUser(user: User?) {
+        if (user == nil) {
+            return
+        }
+        do {
+            let userData = try JSONEncoder().encode(user)
+            UserDefaults.standard.set(userData, forKey: "user")
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
     
     func register(username: String, password: String, completion: @escaping(String, Int) -> ()) {
         let url = URL(string: api + "/auth/register")!
@@ -26,9 +48,7 @@ final class UserViewModel: ObservableObject {
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpBody = jsonData
-        
-        print(request)
-        
+                
         let session = URLSession.shared
         session.dataTask(with: request) { (data, response, error) in
             if error == nil, let data = data, let response = response as? HTTPURLResponse {
@@ -38,5 +58,38 @@ final class UserViewModel: ObservableObject {
                 completion(error!.localizedDescription, 404)
             }
         }.resume()
+    }
+    
+    func login(username: String, password: String, completion: @escaping(String, Int, User?) -> ()) {
+        let url = URL(string: api + "/auth/login")!
+        var request = URLRequest(url: url)
+        
+        let json: [String: Any] = ["username": username, "password": password]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+                
+        var user: User?
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if error == nil, let data = data, let response = response as? HTTPURLResponse {
+                
+                if (response.statusCode != 200) {
+                    let apiResponse = try? JSONDecoder().decode(Response.self, from: data)
+                    completion(apiResponse!.msg, response.statusCode, nil)
+                    return
+                }
+                
+                user = try? JSONDecoder().decode(User.self, from: data)
+                completion("Login successfully", response.statusCode, user)
+                
+            } else {
+                completion(error!.localizedDescription, 404, nil)
+            }
+        }.resume()
+        
+        saveUser(user: user)
     }
 }
