61665e882cc4c3e38a1db09c53347036027ea722
quangan186 <61352012+quangan186@users.noreply.github.com>
2022-09-07 17:59:49 +0700

    search animation

--- My Dictionary/Components/MainViewComponents/SearchBar.swift
+++ My Dictionary/Components/MainViewComponents/SearchBar.swift
@@ -8,25 +8,50 @@
 import SwiftUI
 
 struct SearchBar: View {
-    @Binding var word: String
-        var body: some View {
-            HStack{
-                Image(systemName: "magnifyingglass")
-                    .frame(width: 30, height: 30)
-                    .foregroundColor(Color("Retro-Red"))
-                    .padding(.horizontal)
-                TextField("Search here", text: $word)
-                    .foregroundColor(.black)
-            }
-            .frame(height: 60)
-            .background(Color("Retro-Gray"))
-            .cornerRadius(15)
-            .padding(20)
+    @Binding var input: String
+    @Binding var searchBarTouched:Bool
+    var body: some View {
+        //            HStack{
+        //                Image(systemName: "magnifyingglass")
+        //                    .frame(width: 30, height: 30)
+        //                    .foregroundColor(Color("Retro-Red"))
+        //                    .padding(.horizontal)
+        //                TextField("Search here", text: $word)
+        //                    .foregroundColor(.black)
+        //            }
+        //            .frame(height: 60)
+        //            .background(Color("Retro-Gray"))
+        //            .cornerRadius(15)
+        //            .padding(20)
+        
+        HStack {
+            TextField("Search here", text: $input)
+                .padding()
+                .padding(.horizontal, 30)
+                .background(Color("Retro-Gray")).foregroundColor(.black)
+                .cornerRadius(7.5)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(Color("Retro-Red"))
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding()
+                    }
+                )
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    withAnimation (.linear(duration: 0.25)){
+                        searchBarTouched = true
+                    }
+                }
         }
+        
+        
+    }
 }
 
 struct SearchBar_Previews: PreviewProvider {
     static var previews: some View {
-        SearchBar(word: .constant(""))
+        SearchBar(input: .constant(""), searchBarTouched: .constant(false))
     }
 }
--- My Dictionary/View/MainView.swift
+++ My Dictionary/View/MainView.swift
@@ -13,49 +13,58 @@ struct MainView: View {
     @EnvironmentObject var userVM: UserViewModel
     
     @State var input = ""
-//    @State var user = User(id: "1", username: "Phi", searchedWords: ["favorite", "content", "word"], favoriteWords: [])
+    //    @State var user = User(id: "1", username: "Phi", searchedWords: ["favorite", "content", "word"], favoriteWords: [])
     
     @State private var user: User?
+    @State var searchBarTouched = false
     
     var body: some View {
         NavigationView{
-            VStack{
-                Spacer()
-                Header()
-                Spacer()
-                    .frame(height: 60)
-                
-                // Create searchView for this one before putting routing into it!
-                SearchBar(word: $input)
-                
-                Button(action: {
-                    // Change to WordListHistory view
-                    viewRouting.state = .historyView
-                }, label: {
-                    Text("Search history >>")
-                        .foregroundColor(Color("Retro-Gray"))
-                })
-                
-                Button(action: {
-                    // Change to WordListHistory view
-                    viewRouting.state = .userSetting
-                }, label: {
-                    Text("Setting >>")
-                        .foregroundColor(Color("Retro-Gray"))
-                })
-                Button(action: {
-                    // Change to WordListHistory view
-                    viewRouting.state = .test
-                }, label: {
-                    Text("Setting >>")
-                        .foregroundColor(Color("Retro-Gray"))
-                })
-                Spacer()
-            }
-            .navigationBarHidden(true)
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-            .background(Color("Hard-purple"))
-            .edgesIgnoringSafeArea(.all)
+            GeometryReader{
+                g in
+                VStack{
+                    Spacer()
+                    Header().disabled(searchBarTouched).opacity(searchBarTouched ? 0 : 1)
+                    Spacer()
+                        .frame(height: 60)
+                    
+                    // Create searchView for this one before putting routing into it!
+                    
+                    SearchBar(input: $input, searchBarTouched: $searchBarTouched).offset(x: 0, y: searchBarTouched ? -(g.size.height/3) : 0)
+                    
+                    Button(action: {
+                        // Change to WordListHistory view
+                        viewRouting.state = .historyView
+                    }, label: {
+                        Text("Search history >>")
+                            .foregroundColor(Color("Retro-Gray"))
+                    }).disabled(searchBarTouched).opacity(searchBarTouched ? 0 : 1)
+                    
+                    Button(action: {
+                        // Change to WordListHistory view
+                        viewRouting.state = .userSetting
+                    }, label: {
+                        Text("Setting >>")
+                            .foregroundColor(Color("Retro-Gray"))
+                    }).disabled(searchBarTouched).opacity(searchBarTouched ? 0 : 1)
+                    Button(action: {
+                        // Change to WordListHistory view
+                        viewRouting.state = .test
+                    }, label: {
+                        Text("Setting >>")
+                            .foregroundColor(Color("Retro-Gray"))
+                    }).disabled(searchBarTouched).opacity(searchBarTouched ? 0 : 1)
+                    Spacer()
+                }
+            }.navigationBarHidden(true)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                .background(Color("Hard-purple"))
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture {
+                    withAnimation (.linear(duration: 0.25)){
+                        searchBarTouched = false
+                    }
+                }
         }
         .navigationViewStyle(StackNavigationViewStyle())
     }
