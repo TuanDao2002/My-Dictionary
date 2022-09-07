4b3ec56107f24f44477ed23a6afc16b6ffb23efa
quangan186 <61352012+quangan186@users.noreply.github.com>
2022-09-08 00:26:20 +0700

    Link search to word content view

--- My Dictionary/Components/MainViewComponents/SearchBar.swift
+++ My Dictionary/Components/MainViewComponents/SearchBar.swift
@@ -10,6 +10,11 @@ import SwiftUI
 struct SearchBar: View {
     @Binding var input: String
     @Binding var searchBarTouched:Bool
+    @EnvironmentObject var userVM: UserViewModel
+    @EnvironmentObject var wordVM: WordViewModel
+    @State private var msg: String = ""
+    @State var word: Word?
+    @EnvironmentObject var viewRouting: ViewRouting
     var body: some View {
         //            HStack{
         //                Image(systemName: "magnifyingglass")
@@ -23,30 +28,41 @@ struct SearchBar: View {
         //            .background(Color("Retro-Gray"))
         //            .cornerRadius(15)
         //            .padding(20)
-        
-        HStack {
-            TextField("Search here", text: $input)
-                .padding()
-                .padding(.horizontal, 30)
-                .background(Color("Retro-Gray")).foregroundColor(.black)
-                .cornerRadius(7.5)
-                .overlay(
-                    HStack {
-                        Image(systemName: "magnifyingglass")
-                            .foregroundColor(Color("Retro-Red"))
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                            .padding()
+        VStack{
+            HStack {
+                TextField("Search here", text: $input)
+                    .padding()
+                    .padding(.horizontal, 30)
+                    .background(Color("Retro-Gray")).foregroundColor(.black)
+                    .cornerRadius(7.5)
+                    .overlay(
+                        HStack {
+                            Image(systemName: "magnifyingglass")
+                                .foregroundColor(Color("Retro-Red"))
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                                .padding().onTapGesture {
+                                    wordVM.getWordDefinition(searchedWord: input) { msg, word in
+                                        self.msg = msg
+                                        self.word = word
+                                    }
+                                }
+                        }
+                    )
+                    .padding(.horizontal, 10)
+                    .onTapGesture {
+                        withAnimation (.linear(duration: 0.25)){
+                            searchBarTouched = true
+                        }
                     }
-                )
-                .padding(.horizontal, 10)
-                .onTapGesture {
-                    withAnimation (.linear(duration: 0.25)){
-                        searchBarTouched = true
-                    }
+            }
+            if msg == "Word found"{
+                Button {
+                    viewRouting.state = .wordView
+                } label: {
+                    WordRow(title: word?.word ?? "", userVM: userVM).padding(10)
                 }
+            }
         }
-        
-        
     }
 }
 
